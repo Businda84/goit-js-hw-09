f472081b4e43ec0274edfcda7d74f40dfe2ce08f
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-06-25 20:16:35 +0300

    HW js 9 Task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,81 @@
+
+import flatpickr from "flatpickr";
+import Notiflix from 'notiflix';
+import "flatpickr/dist/flatpickr.min.css";
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+    minuteIncrement: 1,
+  
+    onClose(selectedDates) {
+        if (selectedDates[0]<=currentTime) {
+           Notiflix.Notify.failure("Please choose a date in the future");
+        } else startBtn.disabled = false;
+  console.log(selectedDates[0]);
+  },
+};
+
+const inputDateEl = document.getElementById('datetime-picker')
+const startBtn = document.querySelector('[data-start]')
+const refs = {
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('data-hours'),
+    minutes: document.querySelector('data-minutes'),
+    seconds:document.querySelector('data-seconds'),
+}
+const delay = 1000;
+startBtn.disabled = true;
+// let currentTime = null;
+let currentTime = new Date();
+
+
+flatpickr(inputDateEl, options);
+
+
+startBtn.addEventListener('click', clickBtnStart);
+function clickBtnStart() {
+    intervalId = setInterval(() => {
+        timer();
+    },delay)
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+function timer() {
+    if (currentTime === null || intervalId === null) {
+        return;
+    }
+    currentTime -= delay;
+    if (currentTime < 0) {
+        clearInterval(intervalId);
+        intervalId = null;
+        return;
+    }
+
+}
+   
+function addLeadingZero(value){
+return String(value).padStart(2, '0');
+}
+console.log(convertMs);
+function updateTime(value){}
