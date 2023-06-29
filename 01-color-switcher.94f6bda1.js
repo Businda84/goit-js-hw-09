2d6aac8aa38fb1122d7d6ddf8032e6b9b4080877
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-06-29 16:52:50 +0000

    Deploying to gh-pages from @ Businda84/goit-js-hw-09@56bfaaeea4dd723fd51a96d5b53b54aa7666644a 🚀

--- 01-color-switcher.94f6bda1.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let n=null;e.addEventListener("click",(function(){n=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){return e.disabled=!1,d.disabled=!0,void clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.94f6bda1.js.map
