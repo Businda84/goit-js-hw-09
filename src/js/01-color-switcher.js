56bfaaeea4dd723fd51a96d5b53b54aa7666644a
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-06-29 19:52:14 +0300

    with corrections

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,7 +10,7 @@ function getRandomHexColor() {
              return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
         }
 
-
+stopBtnEl.disabled = true;
 function onClickStart() {
     
   timer=setInterval(() => {
